d3b864ae5545a8a2ce516dfd5d190861bbae811f
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2024-11-14 12:49:02 -0500

    done all logic

--- src/index.js
+++ src/index.js
@@ -2,12 +2,33 @@ import { fetchBreeds, fetchCatByBreed } from './cat-api-service';
 
 const selectEl = document.querySelector('.breed-select');
 const conteinerEl = document.querySelector('.cat-info');
+const showLoadEl = document.querySelector('.loader');
+const showErrorEl = document.querySelector('.error');
+
+let isLoading = false;
+let isError = false;
 
 selectEl.addEventListener('change', e => {
+  isLoading = true;
+  toggleLoading();
   const catId = e.target.value;
-  fetchCatByBreed(catId).then(({ data }) => {
-    catInfoMarkup(data);
-  });
+  fetchCatByBreed(catId)
+    .then(({ data }) => {
+      console.log(data);
+      if (data.length === 0) {
+        throw new Error();
+      }
+      isLoading = false;
+      toggleLoading();
+      catInfoMarkup(data);
+    })
+    .catch(err => {
+      console.log(err);
+      isLoading = false;
+      toggleLoading();
+      isError = true;
+      showErrorMsg();
+    });
 });
 
 fetchBreeds()
@@ -17,6 +38,24 @@ fetchBreeds()
   .catch(err => {
     console.error(err);
   });
+
+function showErrorMsg() {
+  if (!isError) {
+    return;
+  }
+  showErrorEl.style.display = 'block';
+  selectEl.style.display = 'none';
+}
+
+function toggleLoading() {
+  if (!isLoading) {
+    showLoadEl.style.display = 'none';
+    conteinerEl.style.display = 'block';
+    return;
+  }
+  showLoadEl.style.display = 'block';
+  conteinerEl.style.display = 'none';
+}
 
 function selectMarkup(data) {
   const markup = data
@@ -40,7 +79,3 @@ function catInfoMarkup(data) {
     .join('');
   return conteinerEl.insertAdjacentHTML('afterbegin', markup);
 }
-
-// console.log(breeds[0].description);
-// console.log(breeds[0].temperament);
-// console.log(url);
